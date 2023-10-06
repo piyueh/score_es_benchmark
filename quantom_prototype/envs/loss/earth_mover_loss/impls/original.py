bc12ab2cc70a67da680b0963523e380f85bd8966
Pi-Yueh Chuang <pychuang@vt.edu>
2023-10-05 22:48:57 -0400

    clean old Python code

--- quantom_prototype/envs/loss/earth_mover_loss/impls/original.py
+++ quantom_prototype/envs/loss/earth_mover_loss/impls/original.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # vim:fenc=utf-8
 
-"""Original score_es using NumPy and SciPy
+"""Earth-Mover loss implementations similiar to the original NumPy code.
 """
 import numpy
 import scipy
@@ -9,115 +9,142 @@ import pylibraft.distance
 import cupy
 import cupyx.scipy.spatial
 import torch
-from numba import njit
+import numba
 
 
-def score_es_original_numpy(preds, obsrvs):
-    """
-    Computes the energy score between the predicted and observed events.
+def earth_mover_numpy(preds, obsrvs):
+    """Computes the energy score between the predicted and observed events.
+
+    Notes
+    -----
+    This is a simplified version of the original code from Emil C..
 
     Parameters
     ----------
     preds : numpy.ndarray
-    A 2D array of predicted events.
+        A 2D array of predicted events.
 
     obsrvs : numpy.ndarray
-    A 2D array of observed events.
+        A 2D array of observed events.
 
     Returns
     -------
     float
-    The discriminative score between the predicted and observed events.
+        The discriminative score between the predicted and observed events.
     """
-    obs_size, obs_event = obsrvs.shape
-    pred_size, pred_event = preds.shape
-    assert obs_event == pred_event, "Observations and events have different sizes"
+    # sanity checks; can be disabled w/ CMD flag `-O`
+    assert preds.ndim == obsrvs.ndim == 2
+    assert preds.shape[1] == obsrvs.shape[1]  # i.e., nfeatures
 
-    # Calculate score1
-    diff_1 = numpy.linalg.norm(preds[:, None] - obsrvs, ord=2, axis=2)
-    score1 = numpy.mean(diff_1)
+    np = len(preds.shape)
 
-    # Calculate score2
-    if pred_size > 1:
-        diff_2 = numpy.linalg.norm(preds[:, None] - preds, ord=2, axis=2)
-        numpy.fill_diagonal(diff_2, 0)
-        score2 = numpy.sum(diff_2) / (2 * pred_size * (pred_size - 1))
+    # Calculate score1 (preds[:, None].shape = (np, 1, nfeatures))
+    score1 = numpy.linalg.norm(preds[:, None]-obsrvs, ord=2, axis=2)
+    score1 = numpy.mean(score1)
+
+    # Calculate score2 (preds[:, None].shape = (np, 1, nfeatures))
+    if np > 1:
+        score2 = numpy.linalg.norm(preds[:, None]-preds, ord=2, axis=2)
+        numpy.fill_diagonal(score2, 0)  # should already be zero "theoretically"
+        score2 = numpy.sum(score2) / (2 * np * (np - 1))
     else:
         score2 = 0.0
 
     return score1 - score2
 
 
-def score_es_original_scipy(preds, obsrvs):
-    """The fallback of score_es when numba and c implementations are not available.
+def earth_mover_scipy(preds, obsrvs):
+    """Earth-Mover loss using SciPy for pairwise distance matrices.
     """
-    pred_size, pred_event = preds.shape
-    assert obsrvs.shape[1] == pred_event, "Observations and events have different sizes"
+    # sanity checks; can be disabled w/ CMD flag `-O`
+    assert preds.ndim == obsrvs.ndim == 2
+    assert preds.shape[1] == obsrvs.shape[1]  # i.e., nfeatures
+
+    np = len(preds)
 
     # Calculate score1
     score1 = numpy.mean(scipy.spatial.distance.cdist(preds, obsrvs))
 
     # Calculate score2
-    score2 = scipy.spatial.distance.pdist(preds)
-    score2 = numpy.sum(score2) / (pred_size * (pred_size - 1))
+    score2 = numpy.sum(scipy.spatial.distance.pdist(preds))
+    score2 /= (np * (np - 1))  # note pdist only calculate half the matrix
 
     return score1 - score2
 
 
-@njit(nogil=True, boundscheck=False)
-def score_es_original_numba(preds, obsrvs):
-    """Naive Numba impl. Credit: Niteya
+@numba.njit(nogil=True, boundscheck=False)
+def earth_mover_numba(preds, obsrvs):
+    """Naive Numba implementations.
 
     Notes
     -----
-    We tried to mimic the original Python code as much as possible. However, the original NumPy's
-    norm function accepts an argument `axis`, while Numba's own norm implementation does not.
-    We therefore had to modified the code somehow.
+    * Credit: Niteya
+    * We tried to mimic the original Python code as much as possible. However,
+      the original NumPy's norm function accepts an argument `axis`, while
+      Numba's own norm implementation does not. We therefore had to modified the
+      code somehow.
     """
-    obs_size, obs_event = obsrvs.shape
-    pred_size, pred_event = preds.shape
-    assert obs_event == pred_event, "The second dimensions do not match"
+    # sanity checks; can be disabled w/ CMD flag `-O`
+    assert preds.ndim == obsrvs.ndim == 2
+    assert preds.shape[1] == obsrvs.shape[1]  # i.e., nfeatures
 
-    score_1 = 0.0
-    score_2 = 0.0
-    for i in range(pred_size):
-        for j in range(obs_size):
-            score_1 += numpy.linalg.norm(preds[i] - obsrvs[j])
+    no = len(obsrvs)
+    np = len(preds)
 
-        for j in range(i+1, pred_size):
-            score_2 += numpy.linalg.norm(preds[i] - preds[j])
+    score1 = 0.0
+    score2 = 0.0
+    for i in range(np):
+        for j in range(no):
+            score1 += numpy.linalg.norm(preds[i]-obsrvs[j])
 
-    score_1 /= (obs_size * pred_size)
-    score_2 /= (pred_size * (pred_size - 1))
+        for j in range(i+1, np):
+            score2 += numpy.linalg.norm(preds[i]-preds[j])
 
-    return score_1 - score_2
+    score1 /= (no * np)
+    score2 /= (np * (np - 1))
+
+    return score1 - score2
 
 
-def score_es_original_cupy(preds, obsrvs):
-    """Using CuPy.
+def earth_mover_cupy(preds, obsrvs):
+    """Earth-Mover loss using CuPy for pairwise distance matrices.
+
+    Notes
+    -----
+    * This is GPU code.
+    * CuPy's cdist and pdist only support float32 as of Oct 2023
     """
-
+    # sanity checks; can be disabled w/ CMD flag `-O`
+    assert preds.ndim == obsrvs.ndim == 2
+    assert preds.shape[1] == obsrvs.shape[1]  # i.e., nfeatures
     assert preds.dtype == cupy.float32, "CuPy's cdist only supports float32"
     assert obsrvs.dtype == cupy.float32, "CuPy's cdist only supports float32"
 
-    pred_size, pred_event = preds.shape
-    assert obsrvs.shape[1] == pred_event, "Observations and events have different sizes"
+    np = len(preds)
 
     # Calculate score1
     score1 = cupy.mean(cupyx.scipy.spatial.distance.cdist(preds, obsrvs))
 
     # Calculate score2
-    score2 = cupyx.scipy.spatial.distance.pdist(preds)
-    score2 = cupy.sum(score2) / (pred_size * (pred_size - 1))
+    score2 = cupy.sum(cupyx.scipy.spatial.distance.pdist(preds))
+    score2 /= (np * (np - 1))
 
     return score1 - score2
 
 
-def score_es_original_raft(preds, obsrvs):
-    """Using RapidsAI's Raft library.
+def earth_mover_raft(preds, obsrvs):
+    """Earth-Mover loss using Raft for pairwise distance matrices.
+
+    Notes
+    -----
+    * This is GPU code.
+    * The input arrays are CuPy arrays.
     """
-    pred_size, pred_event = preds.shape
-    assert obsrvs.shape[1] == pred_event, "Observations and events have different sizes"
+    # sanity checks; can be disabled w/ CMD flag `-O`
+    assert preds.ndim == obsrvs.ndim == 2
+    assert preds.shape[1] == obsrvs.shape[1]  # i.e., nfeatures
+
+    np = len(preds)
 
     # pre-allocate memory
     out1 = cupy.zeros((pred_size, obsrvs.shape[0]), dtype=preds.dtype)
@@ -131,40 +158,55 @@ def score_es_original_raft(preds, obsrvs):
 
     score1 = out1.mean()
     score2 = out2.sum()
-    score2 /= (2 * pred_size * (pred_size - 1))
+    score2 /= (2 * np * (np - 1))  # note score2 was the full matrix
 
     return score1 - score2
 
 
-def score_es_original_torch(preds, obsrvs):
-    """Using PyTorch.
+def earth_mover_torch(preds, obsrvs):
+    """Earth-Mover loss using PyTorch for pairwise distance matrices.
+
+    Notes
+    -----
+    This function can run on either CPU or GPU, depending on the location of
+    the input tensors.
     """
-    pred_size, pred_event = preds.shape
-    assert obsrvs.shape[1] == pred_event, "Observations and events have different sizes"
+    # sanity checks; can be disabled w/ CMD flag `-O`
+    assert preds.ndim == obsrvs.ndim == 2
+    assert preds.shape[1] == obsrvs.shape[1]  # i.e., nfeatures
+
+    np = len(preds)
 
     # Calculate score1
     score1 = torch.cdist(preds, obsrvs).mean()  # = euclidean
 
     # Calculate score2
     score2 = torch.nn.functional.pdist(preds).sum()  # = euclidean
-    score2 /= (pred_size * (pred_size - 1))
+    score2 /= (np * (np - 1))
 
-    # TODO: synchronization?
     return score1 - score2
 
 
 @torch.jit.script
-def score_es_original_torchscript(preds, obsrvs):
-    """Using PyTorch's TorchScript.
+def earth_mover_torchscript(preds, obsrvs):
+    """Earth-Mover loss using TorchScript for pairwise distance matrices.
+
+    Notes
+    -----
+    This function can run on either CPU or GPU, depending on the location of
+    the input tensors.
     """
-    pred_size, pred_event = preds.shape
-    assert obsrvs.shape[1] == pred_event, "Observations and events have different sizes"
+    # sanity checks; can be disabled w/ CMD flag `-O`
+    assert preds.ndim == obsrvs.ndim == 2
+    assert preds.shape[1] == obsrvs.shape[1]  # i.e., nfeatures
+
+    np = len(preds)
 
     # Calculate score1
-    score1 = torch.cdist(preds, obsrvs).mean()  # = euclidean
+    score1 = torch.cdist(preds, obsrvs).mean()  # euclidean norm
 
     # Calculate score2
-    score2 = torch.nn.functional.pdist(preds).sum()  # = euclidean
-    score2 /= (pred_size * (pred_size - 1))
+    score2 = torch.nn.functional.pdist(preds).sum()  # euclidean norm
+    score2 /= (np * (np - 1))
 
     return score1 - score2
